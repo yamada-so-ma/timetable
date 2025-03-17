71038aec92703fd45b45026f304ee7fc6753d59d
yamada-soma <fki2466288@stu.o-hara.ac.jp>
2025-03-17 16:30:20 +0900

    ファイルを作成した

---
+++ main.mjs
@@ -0,0 +1,20 @@
+import { routes } from "./src/routes.mjs";
+
+async function onHashChange() {
+  const hash = window.location.hash;
+  console.log(hash);
+  if (hash === "") {
+    window.location.hash = "#home";
+  }
+  const page = routes[hash];
+  if (!page) {
+    console.warn("unknown route");
+    return;
+  }
+
+  const appRoot = document.querySelector("app-root");
+  appRoot.innerHTML = page;
+}
+
+window.addEventListener("hashchange", onHashChange);
+onHashChange();
